ca29bef807dd9a84c01b69bfe788e5eae4025276
Djordje Zivanovic <djolezile@gmail.com>
2020-12-17 21:01:33 +0100

    first commit

---
+++ src/main/java/rs/edu/raf/nwp/NwpApplication.java
@@ -0,0 +1,13 @@
+package rs.edu.raf.nwp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwpApplication.class, args);
+    }
+
+}

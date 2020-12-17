a01a2cd79e666b0d4413b314485b9a1c8ce632bd
Djordje Zivanovic <djolezile@gmail.com>
2020-12-17 21:22:01 +0100

    test commit 2

--- src/main/java/rs/edu/raf/nwp/services/IService.java
+++ src/main/java/rs/edu/raf/nwp/services/IService.java
@@ -1,4 +1,15 @@
 package rs.edu.raf.nwp.services;
 
-public interface IService {
+import java.util.List;
+import java.util.Optional;
+
+public interface IService <T, ID> {
+    T save(T var1);
+
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    void deleteById(ID id);
+
 }
